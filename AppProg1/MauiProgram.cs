eff0b1475a7102accdc9b55871e71500cd701acf
Failbreak <DevIsUsingLaptop@NotARealMail.WhyDoYouEnforceThis>
2024-01-15 13:43:11 +0100

    kk

--- AppProg1/MauiProgram.cs
+++ AppProg1/MauiProgram.cs
@@ -12,7 +12,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-
+		//hello
 		return builder.Build();
 	}
 }
